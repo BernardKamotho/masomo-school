4594744b03186458ce64cb9c6733ad4720ea0c15
BenKamotho <105359629+BernardKamotho@users.noreply.github.com>
2025-08-01 11:26:35 +0300

    protected routes

--- src/App.js
+++ src/App.js
@@ -12,21 +12,37 @@ import LoginComponent from './components/LoginComponent';
 import AdminDashboard from './components/admin/AdminDashboard';
 import TeacherDashboard from './components/teacher/TeacherDashboard';
 import ParentDashboard from './components/parent/ParentDashboard';
+import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './context/ProtectedRoute';
+import AdminLayout from './components/admin/AdminLayout';
 
 function App() {
   return (
     <Router>
-      <Routes>
+      <AuthProvider>
+        <Routes>
         <Route path='/' element={<HomeComponent />} />
+
+        {/* Below are admin's Protected Routes */}
+        <Route path='/admin-dashboard' 
+        element={
+          <ProtectedRoute allowedRoles={['admin']}>
+            <AdminLayout />
+          </ProtectedRoute>
+        }>
+          <Route path='' element={ <AdminDashboard/> } />
+        </Route>
+
+
         <Route path='/register' element={<RegisterComponent />} />
         <Route path='/notAuthorized' element={<NotAuthorized />} />
         <Route path='/login' element={< LoginComponent  />} />
-        <Route path='/admin-dashboard' element={< AdminDashboard />} />
         <Route path='/teacher-dashboard' element={< TeacherDashboard />} />
         <Route path='/parent-dashboard' element={< ParentDashboard />} />
 
         <Route path='*' element={<Notfound />} />
       </Routes>
+      </AuthProvider>
     </Router>
   );
 }
